c79c4f3a89f7cc7b6abe6c6d54ea74aca4a4ff5a
VaradPandharkar <varadpandharkar89@gmail.com>
2026-07-25 10:26:08 +0530

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,58 @@
+/*
+#include<iostream>
+int main(){
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+    return 0;
+}
+*/
+
+            //(INTEGER) (Whole number)
+/*
+#include<iostream>
+int main(){
+    int age = 19;
+    int year = 2026;
+    int days = 7.5;
+    std::cout << days;
+    return 0;
+}
+*/
+
+          // (DOUBLE) (Number including decimals)
+/*
+#include<iostream>
+int main(){
+    double price = 10.88;
+    double gpa = 8.5;
+    double temperature = 25.1;
+
+    std::cout << temperature;
+    return 0;
+}
+*/
+
+           //(SINGLE CHARACTER))
+/*
+#include<iostream>
+int main(){
+    char grade = 'A';
+    char initial = 'B';
+
+    std::cout << initial;
+    return 0;
+}
+*/
+
+         //(Boolean) (True or False)
+#include<iostream>
+int main(){
+    bool Student = false;
+    bool power = true;
+    bool forsale = false;
+    return 0;
+}
