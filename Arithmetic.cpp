a9790581b40a0cbaa1e790b04b9b2a490d7db875
VaradPandharkar <varadpandharkar89@gmail.com>
2026-07-26 11:30:40 +0530

    Add files via upload

---
+++ Arithmetic.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+int main(){
+
+    int students = 20;
+// ADDITION
+    //students = students + 1;
+    //students+=1;
+
+// SUBTRACTION
+    //students = students - 1;
+    students-=1;
+
+// MULTIPLICATION
+    //students = students * 2;
+    //students*=2;
+
+// DIVISION
+    //students = students / 2;
+    //students/=2;
+
+    std::cout << students;
+    return 0;
+}
