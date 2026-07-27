1be4dc37a46816a4284a53a303bb25a8bacc1c3d
VaradPandharkar <varadpandharkar89@gmail.com>
2026-07-27 18:48:48 +0530

    Add files via upload

---
+++ Maths_Function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+int main(){
+    double x = 3.14;
+    double y = 4;
+    double z;
+
+    //z = std::max(x,y);
+    //z = std::min(x,y);
+    //z = pow(2,3);
+    //z = sqrt(64);
+    //z = abs(-3);
+    //z = round(x);
+    //z = ceil(x);
+    z = floor(x);
+
+    std::cout << z;
+    return 0;
+}
