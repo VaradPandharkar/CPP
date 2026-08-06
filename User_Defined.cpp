bba8dc68eac06270615c945bc83c1aaa962ab957
VaradPandharkar <varadpandharkar89@gmail.com>
2026-08-06 19:33:48 +0530

    Add files via upload

---
+++ User_Defined.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+    void happyBirthday(){
+        std::cout << "Happy Birthday to you!\n";
+         std::cout << "Happy Birthday to you!\n";
+          std::cout << "Happy Birthday to you!\n";
+           std::cout << "Happy Birthday to you!\n";
+            std::cout << "Happy Birthday to you!\n\n";
+    }
+int main(){
+    happyBirthday();
+    happyBirthday();
+    happyBirthday();
+return 0;
+}
+    
