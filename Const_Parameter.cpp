55d668fa83f670702ab9706e65a66552155556a5
VaradPandharkar <varadpandharkar89@gmail.com>
2026-08-23 18:37:12 +0530

    Add files via upload

---
+++ Const_Parameter.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+void printInfo(const std::string name, const int age);
+int main(){
+    std::string name = "Varad";
+    int age = 19;
+
+    printInfo(name, age);
+
+    return 0;
+}
+void printInfo(const std::string name, const int age){
+    std::cout << name << '\n';
+    std::cout << age << '\n';
+}
