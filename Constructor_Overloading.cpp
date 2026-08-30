b4de8cde4ce534a0aeb2c6aabf59841f68df14c8
VaradPandharkar <varadpandharkar89@gmail.com>
2026-08-30 10:10:25 +0530

    Add files via upload

---
+++ Constructor_Overloading.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+// Multiple constructor same name but different parameters allow for varrying arguments when instantiating an object
+
+class Pizza{
+    public:
+    std::string topping1;
+
+    Pizza(std::string topping1){
+        this->topping1 = topping1;
+
+    }
+};
+
+int main(){
+    Pizza pizza1("pepperoni");
+    std::cout << pizza1.topping1 << '\n';
+}
